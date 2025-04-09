aff8c4ba47955e9c84f2def07d5cb4e802b0d8f9
Piyush patil <098piyushpatil@gmail.com>
2025-04-10 03:21:05 +0530

    Update .gitignore and fix import in notification routes; correct deleteNotifications function reference

--- backend/routes/notification.routes.js
+++ backend/routes/notification.routes.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import { protectedRoute } from '../middleware/protectedRoute';
+import { protectedRoute } from '../middleware/protectedRoute.js';
+import { deleteNotifications, getNotifications } from '../controller/notification.controller.js';
 
 const router = express.Router();
 
 router.get('/', protectedRoute, getNotifications )
-router.delete('/', protectedRoute, deleteNotification )
+router.delete('/', protectedRoute, deleteNotifications )
 
 export default router;
 
