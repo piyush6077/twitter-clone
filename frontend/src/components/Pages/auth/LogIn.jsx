fa00f693612885c33206b670ca403402a5751a1a
Piyush patil <098piyushpatil@gmail.com>
2025-04-13 04:29:52 +0530

    Enhance Posts component to support dynamic feed types and improve data fetching logic

--- frontend/src/components/Pages/auth/LogIn.jsx
+++ frontend/src/components/Pages/auth/LogIn.jsx
@@ -21,7 +21,7 @@ const LoginPage = () => {
 			const res = await fetch("api/auth/login", {
 				method: "POST",
 				headers: {
-					"Content-Type": "application/json",
+					"Content-Type": "application/json"
 				},
 				body: JSON.stringify({ username, password }),
 				credentials: "include"
