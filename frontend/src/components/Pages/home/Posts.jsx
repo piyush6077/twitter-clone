f4ddbd65bc6feefc2a4c69890a571e377e72dd5b
Piyush patil <098piyushpatil@gmail.com>
2025-04-14 08:57:06 +0530

    implement post deletion functionality

--- frontend/src/components/Pages/home/Posts.jsx
+++ frontend/src/components/Pages/home/Posts.jsx
@@ -54,8 +54,8 @@ const Posts = ({feedType}) => {
 					<PostSkeleton />
 				</div>
 			)}
-			{!isLoading && isRefetching && posts?.length === 0 && <p className='text-center my-4'>No posts in this tab. Switch 👻</p>}
-			{!isLoading && isRefetching && posts && (
+			{!isLoading && !isRefetching && posts?.length === 0 && <p className='text-center my-4'>No posts in this tab. Switch 👻</p>}
+			{!isLoading && !isRefetching && posts && (
 				<div>
 					{posts.map((post) => (
 						<Post key={post._id} post={post} />
